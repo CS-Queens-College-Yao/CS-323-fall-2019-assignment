4d74a100d49cc4727cd3b9074a1c5dcec6c77959
Matthew Verderber <56893220+MatthewVerderber@users.noreply.github.com>
2019-11-24 20:58:45 -0500

    Coded runTrialsRecur and runTrialsBottomUp methods.

--- RunningTrials.java
+++ RunningTrials.java
@@ -9,22 +9,44 @@ public class RunningTrials {
   // Do not change the parameters!
   public int runTrialsRecur(int possibleSpeeds, int days) {
     int minTests = 0;
-    // Your code here
+	boolean hurt = false;
+    if(days == 0) return minTests = 1; 	// trying all speeds in one day
+    if(possibleSpeeds == 1) return minTests = 1;  		//base case
+    minTests++;
+    if(hurt == true)
+    	runTrialsRecur(possibleSpeeds-1, days-1);
+    else runTrialsRecur(possibleSpeeds-1, days);
     return minTests;
   }
 
   // Optional:
   // Pick whatever parameters you want to, just make sure to return an int.
+  /*
   public int runTrialsMemoized() {
     int minTests = 0;
-    // Your optional code here
-    return minTests;
+	boolean hurt = false;                                           // boolean determining whether or not athlete is hurt
+	int[][] testArray = new int[possibleSpeeds][days];				
+	testArray[0][0] = 0;
+	testArray[1][0] = 1;
+	testArray[0][1] = 1;  //initialize base cases								
+    if(days == 0) return testArray[possibleSpeeds][0];				// base case for days
+    if(possibleSpeeds == 0) return minTests = 1; 	 				// base case for possibleSpeeds
+	if(hurt == true)
+		runTrialsRecur(possibleSpeeds-1, days-1);   				    // if athelete is hurt decrement speed and day
+	else runTrialsRecur(possibleSpeeds-1, days); 					// if athelete is unhurt decrement speed
+    minTests++;     												// test complete, increment count
+    return minTests; 
   }
+  **/
 
   // Do not change the parameters!
   public int runTrialsBottomUp(int possibleSpeeds, int days) {
     int minTests = 0;
-    // Your code here
+    int[] testSpeed = new int[possibleSpeeds+1];
+    testSpeed[0] = 1;
+    for(int i = 1; i < possibleSpeeds; i++)
+    	testSpeed[i] = testSpeed[i-1] + 1;
+    minTests = testSpeed[possibleSpeeds-1];
     return minTests;
   }
 
