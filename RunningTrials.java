1f6562d833fc787d9ad0f6f4cd8a389254ea8c88
zhushangs <zhushangs@gmail.com>
2019-11-25 23:25:25 -0500

    Trial file added
    Recursive, memory, DP

--- RunningTrials.java
+++ RunningTrials.java
@@ -7,26 +7,93 @@
 public class RunningTrials {
 
   // Do not change the parameters!
-  public int runTrialsRecur(int possibleSpeeds, int weeks) {
-    int minTests = 0;
-    // Your code here
-    return minTests;
+  public int runTrialsRecur(int possibleSpeeds, int days) {
+	  int minTests = 999;
+
+	  //base case when there is no possibleSpeeds and no days
+	  if(possibleSpeeds == 0 || days == 0) {
+		  return 0;
+	  }
+	  
+	  //base case when there is only one possible speed
+	  if(possibleSpeeds == 1) {
+		  return 1;
+	  }
+	  
+	  for(int i = 2; i <= possibleSpeeds; i++) {
+		  // when the athelete is injured on that day
+		  // when the athlete is not injured on that day
+		  minTests = Math.max(runTrialsRecur(i-1, days-1)+1, runTrialsRecur(possibleSpeeds-i, days)+1);
+	  }
+	    
+	  return minTests;
   }
 
-  // Optional:
+
+// Optional:
   // Pick whatever parameters you want to, just make sure to return an int.
-  public int runTrialsMemoized() {
+  public int runTrialsMemoized(int possibleSpeeds, int days) {
+	int[][] memo = new int[possibleSpeeds+1][days+1];
     int minTests = 0;
+    
     // Your optional code here
-    return minTests;
+    
+    //base case
+	  if(possibleSpeeds == 0 || days == 0) {
+		  return 0;
+	  }
+	  
+	  if(possibleSpeeds == 1) {
+		  return 1;
+	  }
+
+	  if(memo[possibleSpeeds][days] != 0) {
+		  return memo[possibleSpeeds][days];
+	  }
+
+	  for(int i = 2; i <= possibleSpeeds; i++) {
+		  // when the athelete is injured on that day
+		  // when the athlete is not injured on that day
+		  minTests = Math.max(runTrialsMemoized(i-1, days-1)+1, runTrialsMemoized(possibleSpeeds-i, days)+1);
+	  }
+	  
+	  memo[possibleSpeeds][days] = minTests;
+	  
+	  
+	  return memo[possibleSpeeds][days];
   }
 
   // Do not change the parameters!
-  public int runTrialsBottomUp(int possibleSpeeds, int weeks) {
-    int minTests = 0;
-    // Your code here
-    return minTests;
-  }
+  public int runTrialsBottomUp(int possibleSpeeds, int days) {
+	    int minTests = 0;
+	    // Your code here
+	    
+	    int[][] dp = new int[possibleSpeeds+1][days+1];
+	    
+	    for(int i = 1; i<= possibleSpeeds; i++) {
+	    	dp[i][0] = 0;
+	    }
+	    
+	    for(int j = 1; j<= days; j++) {
+	    	dp[0][j] = 0;
+	    	dp[1][j] = 1;
+	    }
+	    
+	    for(int i = 2; i <= possibleSpeeds; i++) {
+	    	for(int j = 2; j <= days; j++) {
+	    		dp[i][j] = 999;
+	    		for(int k = 1; k < i; k++) {
+	    			// when the athelete is injured
+	    			// when the athelete is not injured
+	    			minTests = Math.max(dp[i-1][j-1], dp[i-k][j])+1;
+	    			dp[i][j] = Math.min(dp[i][j], minTests);
+	    			minTests = dp[i][j];
+	    		}
+	    	}
+	    }
+	    
+	    return minTests;
+	  }
 
   public static void main(String args[]){
       RunningTrials running = new RunningTrials();
@@ -36,7 +103,8 @@ public class RunningTrials {
       int minTrials1Bottom = running.runTrialsBottomUp(12, 5);
       int minTrials2Recur = running.runTrialsRecur(20, 8);
       int minTrials2Bottom = running.runTrialsBottomUp(20, 8);
-      System.out.println("12 speeds, 5 weeks: " + minTrials1Recur + " " + minTrials1Bottom);
-      System.out.println("20 speeds, 8 weeks: " + minTrials2Recur + " " + minTrials2Bottom);
+      int minTrials2Memo = running.runTrialsMemoized(20, 8);
+      System.out.println("12 speeds, 5 days: " + minTrials1Recur + " " + minTrials1Bottom);
+      System.out.println("20 speeds, 8 days: " + minTrials2Recur + " " + minTrials2Bottom + " " + minTrials2Memo);
   }
 }
