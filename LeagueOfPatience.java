8ce9e2e02a38793428ae131b2ceecae6453f45a6
ismailfarhan01 <ismailfarhan01@gmail.com>
2019-12-01 14:15:10 -0500

    Update LeagueOfPatience.java

--- LeagueOfPatience.java
+++ LeagueOfPatience.java
@@ -4,8 +4,9 @@ import java.util.Calendar;
 
 /**
  * LeagueOfPatience
- * Author: Your Name and Carolyn Yao
+ * Author: Ismail Farouqe and Carolyn Yao
  * Does this compile or finish running within 5 seconds? Y/N
+ *Y
  */
 
 /**
@@ -41,6 +42,50 @@ public class LeagueOfPatience {
     // You will find the getNextQuestTime method and the minutesBetween method helpful.
     // You can also make new helper methods.
 
+    // code here
+
+    // processed[i] will true if vertex i's shortest time is already finalized
+    Boolean[] processed = new Boolean[durations.length];
+    Date tempTime;
+    // Initialize all distances as INFINITE and processed[] as false
+    for (int v = 0; v < durations.length; v++) {
+      times[v] = Integer.MAX_VALUE;
+      processed[v] = false;
+    }
+
+    // Distance of source vertex from itself is always 0
+    times[S] = 0;
+    // Find shortest path to all the vertices
+    for (int count = 0; count < durations.length-1; count++) {
+      // Pick the minimum distance vertex from the set of vertices not yet processed.
+      // u is always equal to source in first iteration.
+      // Mark u as processed.
+      int u = findNextToProcess(times, processed);
+      processed[u] = true;
+
+      // Update time value of all the adjacent vertices of the picked vertex.
+      for (int v = 0; v <= T; v++) {
+    	  
+        // Update time[v] only if is not processed yet, there is an edge from u to v,
+        // and total weight of path from source to v through u is smaller than current value of time[v]
+    	
+    	// Adding next available quest time, only to the directed vertex
+    	// so only when durations[u][v] != 0 we will add the random number to increase duration.
+    	tempTime = getNextQuestTime(startTime, u, v);
+    	if (durations[u][v] != 0)
+    	{
+    		// above mentioned code calculates time difference between start date and next quest date and converts it into minutes
+    		durations[u][v] += (int)((tempTime.getTime() - startTime.getTime()) / 60000);
+    	}
+    	
+        if (!processed[v] && durations[u][v] != 0 && times[u] != Integer.MAX_VALUE && times[u]+durations[u][v] < times[v]) {
+          times[v] = times[u] + durations[u][v];
+          // updating the startTime when the player in on the new vertex
+          startTime = tempTime;
+        }
+      }
+    }
+    
     printShortestTimes(times);
 
     // Extra Credit: Code below to print the suggested play path i.e. "2, 4, 3, 5"
