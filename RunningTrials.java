57773ed21f4b844fe5b723666b3eee82da0bad8c
Luis Santos <luissantos216@gmail.com>
2019-12-01 16:21:23 -0500

    Finished problem 1, Running Trials and Tribulations

--- RunningTrials.java
+++ RunningTrials.java
@@ -1,3 +1,5 @@
+package cs323;
+
 /**
  * Running Trials
  * Author: Your Name and Carolyn Yao
@@ -9,7 +11,31 @@ public class RunningTrials {
   // Do not change the parameters!
   public int runTrialsRecur(int possibleSpeeds, int days) {
     int minTests = 0;
+    
     // Your code here
+    
+    // If there is only one day there are only as many trials as possible speeds, n.
+    if (days == 1) {
+    	return possibleSpeeds;
+    }
+    
+    // If there is only 1 or 0 speeds then only 1 trial needs to be done or 0.
+    if (possibleSpeeds == 1 || possibleSpeeds == 0){
+    	return possibleSpeeds;
+    }
+    
+    minTests = Integer.MAX_VALUE;
+    int temp;
+    // Two cases either the runner gets injured or the runner does not get injured.
+    // If the runner gets injured at a certain speed, n, then you a day and check the next speed below it.
+    // If the runner does not get injured at a certain speed, n,  then there is no need to check the speeds below it
+    // so you can just check the speeds above it. You do this for every speed.
+    for(int n = 1; n <= possibleSpeeds; n++){
+    	temp = 1 + Math.max(runTrialsRecur(n - 1, days - 1), runTrialsRecur(possibleSpeeds - n, days));
+    	minTests = Math.min(temp, minTests);
+
+    }
+ 
     return minTests;
   }
 
@@ -24,7 +50,35 @@ public class RunningTrials {
   // Do not change the parameters!
   public int runTrialsBottomUp(int possibleSpeeds, int days) {
     int minTests = 0;
+    
     // Your code here
+    
+    // N*M 2d array
+    int [][] solution = new int [possibleSpeeds + 1][days + 1];
+    
+    // Base cases where speed is equal to 0 or 1
+    for(int m = 1; m <= days; m++){
+    	solution[0][m] = 0;
+    	solution[1][m] = 1;
+    }
+    
+    // Base case where days is equal to 1 so all trials are set to n speeds.
+    for (int n = 1; n <= possibleSpeeds; n++){
+    	solution[n][1] = n;
+    }
+    
+    
+    for(int n = 2; n <= possibleSpeeds; n++){
+    	for (int m = 2; m <= days; m++){
+    		solution[n][m] = Integer.MAX_VALUE;
+    		int temp;
+    		for (int k = 1; k <= n; k++) {
+    			temp = 1 + Math.max(solution[k - 1][m - 1], solution[n-k][m]);
+    			solution[n][m] = Math.min(temp, solution[n][m]);
+    		}
+    	}
+    }
+    minTests = solution[possibleSpeeds][days];
     return minTests;
   }
 
