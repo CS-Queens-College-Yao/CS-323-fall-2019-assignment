f2a78c3ec9efe6048ae7a22b637b62693a64e2ac
suzuhe481 <56882831+suzuhe481@users.noreply.github.com>
2019-12-01 00:08:09 -0500

    Added code to HolidaySpecial.java
    Added code to answer question 2C with a non-optimal greedy algorithm. Also included an attempt to code an optimal greedy algorithm.

--- HolidaySpecial.java
+++ HolidaySpecial.java
@@ -1,6 +1,6 @@
 /**
  * HolidaySpecial
- * Author: Your Name and Carolyn Yao
+ * Author: Hector Suazo and Carolyn Yao
  * Does this compile or finish running within 5 seconds? Y/N
  */
 
@@ -38,7 +38,158 @@ public class HolidaySpecial {
     int[][] scheduleTable = new int[numCooks + 1][numSteps + 1];
 
     // Your code here
-
+    
+    /*
+    //Prints signUpTable array for debug
+    System.out.println("signUpTable");
+    for(int i = 1; i < signUpTable.length; i++) {
+    	System.out.print("[");
+    	for(int j = 1; j < signUpTable[0].length; j++) {
+    		System.out.print(" " + signUpTable[i][j] + " ");
+    	}
+    	System.out.println("]");
+    }
+    */
+    
+    
+    
+    //Non-optimal greedy method
+    int numSwitches = 0;	//Probably not needed
+    int currCook = 0;
+    
+    //Assigns an initial cook for step 1
+	for(int i = 1; i < numCooks; i++) {
+		if(signUpTable[i][1] == 1) {
+			currCook = i;
+			scheduleTable[currCook][1] = 1;
+			//System.out.println("Assigning step 1 to cook " + currCook);	//DEBUG
+			break;
+		}
+		else {
+			//NOTHING
+		}
+	}
+	
+	//Assigns all remaining steps to all remaining cooks
+	for(int i = 2; i <= numSteps; i++) {	//Iterates through steps
+		//Checks to see if current cook can do following step, i.
+		if(signUpTable[currCook][i] == 1) {
+			scheduleTable[currCook][i] = 1;
+			//System.out.println("Assigning step " + i + " to cook " + currCook);	//Debug
+		}
+		else {
+			//Checks to see which cook, j, can do the following step, i
+			for(int j = 1; j <= numCooks; j++) {	//Iterates through cooks
+				if(signUpTable[j][i] == 1) {
+					currCook = j;
+					scheduleTable[currCook][i] = 1;
+					numSwitches++;
+					break;
+				}
+			}
+			//System.out.println("Assigning step " + i + " to cook " + currCook);	//Debug
+		}
+	}
+	
+	
+	/*
+	//Prints scheduleTable array for debug
+	System.out.println("scheduleTable");
+    for(int i = 1; i < scheduleTable.length; i++) {
+    	System.out.print("[");
+    	for(int j = 1; j < scheduleTable[0].length; j++) {
+    		System.out.print(" " + scheduleTable[i][j] + " ");
+    	}
+    	System.out.println("]");
+    	
+    }
+    System.out.println();
+	*/
+    
+	/*
+    //Optimal Greedy Algorithm
+    int currStep = 1;
+    int countStep = 0;
+    int maxStep = 0;
+    int maxCook = 0;
+    int[] maxStepArr = new int[numCooks+1];
+    for(int i = 0; i <= numCooks; i++) {
+    	maxStepArr[i] = 0;
+    }
+    boolean scheduleDone = false;
+    
+    while(scheduleDone != true) {
+    	//Searches to see which cook can do most consecutive steps by filling maxStepArr
+    	//System.out.println("Search starting at currStep: " + currStep);		//Debug
+    	for(int cook = 1; cook <= numCooks; cook++) {
+    		for(int step = currStep; step <= numSteps; step++) {
+    			if(signUpTable[cook][step] == 1) {
+    				countStep++;
+    				//System.out.println("Cook " + cook + " can do step " + step + " (" + countStep + ")");		//Debug
+    			}
+    			else {
+    				//System.out.println("maximum steps for cook " + cook + " is " + countStep);	//Debug
+    				//System.out.println("test- currStep: " + currStep + ", countStep: " + countStep + ", step: " + step);	//Debug
+    				maxStepArr[cook] = countStep;
+    				countStep = 0;
+    				break;
+    			}
+    		}
+    	}
+    	
+    	/*	//Debug prints maxStepArr
+    	for(int i = 1; i < maxStepArr.length; i++) {
+    		System.out.println("maxStepArr[" + i + "]: " + maxStepArr[i]);
+    	}
+    	*/
+    	
+		/*
+    	//Finds maxCook and maxSteps from maxStepArr[]
+    	System.out.println("Finding maxCook and maxSteps");
+    	for(int cook = 1; cook <= numCooks; cook++) {
+    		if(maxStepArr[cook] > maxStep) {
+    			//System.out.println("Cook " + cook + " is current max cook");		//Debug
+    			maxCook = cook;
+    			maxStep = maxStepArr[cook];
+    			
+    		}
+    	}
+    	//System.out.println("maxCook is: " + maxCook + " with " + maxStep + " steps");	//Debug
+    	
+    	//Debug - prints variables
+    	//System.out.println("currStep: " + currStep);
+    	//System.out.println("maxStep: " + maxStep);
+    	//System.out.println("scheduleTable filled from steps " + currStep + 
+    					   " to " + (currStep+maxStep-1) + 
+    					   " for cook " + maxCook);
+		
+		
+		
+    	//Fills scheduleTable with the cook that can do the max number of consecutive steps
+    	for(int step = currStep; step < (currStep + maxStep); step++) {
+    		scheduleTable[maxCook][step] = 1;
+    		//System.out.println("scheduleTable filled");	Debug
+    	}
+    	
+    	//Resets some variables and gets them ready for next while loop
+    	currStep = currStep + maxStep;
+    	maxCook = 0;
+    	maxStep = 0;
+    	countStep = 0;
+    	for(int i = 0; i <= numCooks; i++) {
+        	maxStepArr[i] = 0;
+        }
+    	//System.out.println("currStep: " + currStep);	Debug
+    	//System.out.println("numSteps: " + numSteps);	Debug
+    	
+    	//Checks if all steps were checked to exit while loop
+    	if(currStep >= numSteps) {
+    		scheduleDone = true;
+    		//System.out.println("scheduleTable done");	//Debug
+    	}
+    }
+    */
+    
     return scheduleTable;
   }
 
