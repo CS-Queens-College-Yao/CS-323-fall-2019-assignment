47a2133d104b13720abe22e15f42092d223ccc87
Fockery <56888166+Fockery@users.noreply.github.com>
2019-12-01 19:10:34 -0500

    Update RunningTrials.java

--- RunningTrials.java
+++ RunningTrials.java
@@ -9,8 +9,21 @@ public class RunningTrials {
   // Do not change the parameters!
   public int runTrialsRecur(int possibleSpeeds, int days) {
     int minTests = 0;
-    // Your code here
-    return minTests;
+    if(possibleSpeeds <= 1 || days==1 || days==0)		
+        return days;
+  	
+    minTests = Integer.MAX_VALUE;
+    int result = 0;
+    
+    // To find the converging point, comparison between possibleSpeeds = possibleSpeeds - 1 and days = days - 1 
+    for(int i = 1; i <= days; i++)
+    {
+    	result = Math.max(runTrialsRecur(possibleSpeeds - 1, i - 1), runTrialsRecur(possibleSpeeds, days - i));
+    	if(result < minTests)
+    		minTests = result;
+    }
+    	
+    return minTests + 1;
   }
 
   // Optional:
@@ -24,7 +37,32 @@ public class RunningTrials {
   // Do not change the parameters!
   public int runTrialsBottomUp(int possibleSpeeds, int days) {
     int minTests = 0;
-    // Your code here
+    int array[][] = new int[possibleSpeeds + 1][days + 1];
+	    for(int i = 1; i <= possibleSpeeds; i++){
+	    	array[i][0] = 0;
+	    	array[i][1] = 1;
+	    }
+	
+	    // Filling up for the case when days = 1, and have to run full number of tests.
+	    for(int i = 1; i <= days; i++){
+	    	array[1][i] = i;
+	    }
+	    
+	    for (int i = 2; i <= possibleSpeeds; i++){ 
+	        for (int j = 2; j <= days; j++){ 
+	        	// Max Integer to avoid problems with below comparison set up
+	        	array[i][j] = Integer.MAX_VALUE;
+	        	
+	            // Same optimal substructure of the recursion code above
+	            for (int k = 1; k <= j; k++){ 
+	                 int result = 1 + Math.max(array[i - 1][k - 1], array[i][j - k]); 
+	                 if (result < array[i][j])
+	                	 array[i][j] = result; 
+	            } 
+	        } 
+	    } 
+	    
+	    minTests = array[possibleSpeeds][days];
     return minTests;
   }
 
