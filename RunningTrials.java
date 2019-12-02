81608958235c2f8b67d201552e5b07cc0f4d0b78
Reveka-Lee <55007255+Reveka-Lee@users.noreply.github.com>
2019-12-01 20:48:14 -0500

    Update RunningTrials.java

--- RunningTrials.java
+++ RunningTrials.java
@@ -2,7 +2,7 @@
  * Running Trials
  * Author: Reveka Lee and Carolyn Yao
  * Does this compile or finish running within 5 seconds? Y/N
- N
+DOES NOT COMPILE. See code.
  */
 
 public class RunningTrials {
