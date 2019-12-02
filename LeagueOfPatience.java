3fe9f7c6dc0a02a67fc0288b485c25ca3068ed89
Chuhui Chen <38293820+gonese@users.noreply.github.com>
2019-12-01 20:33:43 -0500

    Update LeagueOfPatience.java

--- LeagueOfPatience.java
+++ LeagueOfPatience.java
@@ -40,10 +40,91 @@ public class LeagueOfPatience {
     // Feel free to borrow code from any of the existing methods.
     // You will find the getNextQuestTime method and the minutesBetween method helpful.
     // You can also make new helper methods.
+    int end= T;
+    int start = S;
+    // This is the array where we'll store all the final shortest times
+    int[] times = new int[numVertices];
+    // processed[i] will true if vertex i's shortest time is already finalized
+    Boolean[] processed = new Boolean[numVertices];
+    // we store our path in path
+    String path="";
+    //store value in our minimal path in pathBloack
+    int[][] pathBlock = new int[numVertices][numVertices];
 
-    printShortestTimes(times);
+     //Update cost with wait time
+     for(int i=0;i<numVertices;i++)
+       for(int j=0;j<numVertices;j++){
+         int wait = minutesBetween(startTime,getNextQuestTime(startTime,i,j))/60;
+         if(durations[i][j]!=0)
+           durations[i][j]=durations[i][j]+wait;
+       }
+      //using from variable as row to update pathBlock
+     int from;
+
+     for (int v = 0; v < numVertices; v++) {
+       times[v] = Integer.MAX_VALUE;
+       processed[v] = false;
+      }
+     // Distance of source vertex from itself is always 0
+     times[S] = 0;
+     // Find shortest path to all the vertices
+     for (int count = 0; count < numVertices - 1 ; count++) {
+     // Pick the minimum distance vertex from the set of vertices not yet processed.
+     // u is always equal to source in first iteration.
+     // Mark u as processed.
+       S = findNextToProcess(times, processed);
+       processed[S] = true;
+       from = S;
+     // Update time value of all the adjacent vertices of the picked vertex.
+       for (int v = 0; v < numVertices; v++) {
+        // Update time[v] only if is not processed yet, there is an edge from u to v,
+        // and total weight of path from source to v through u is smaller than current value of time[v]
+        // we are also going to update pathBlock in order to track actual path using the same logic.
+         if (!processed[v] && durations[S][v]!=0 && times[S] != Integer.MAX_VALUE && times[S]+durations[S][v]< times[v]) {
+           times[v] = times[S] + durations[S][v];
+           pathBlock[from][v]=times[v];
+         }
+        }
+      }
+     // start from the destination, trackthe path backward until i get to the starting point
+     // we also need a count variable in case there is no path from start to end.
+     
+     int count=0;
+     //keep updating path until we reach end
+     while(end != start){
+     // If there is no path exists, count is going to be greater than numVertices
+     // in that case, we are going to break the loop
+       if(count>numVertices) {
+         path="no path";
+         break;
+       }
+     //inital possible to store the minimal value in pah
+       int possible = Integer.MAX_VALUE;
+     //append stage we visited to path
+       path=" -> "+ end+path;	
+     //find the minimal cost path
+       for(int i=0;i<numVertices;i++) {
+     // If there exist a path and cost is less than possible
+     // we are going to store that value in possible
+     // we are also going to update end for the next iteration
+         if(pathBlock[i][end]!= 0&&pathBlock[i][end]<possible) {
+           possible=pathBlock[i][end];
+           end=i;
+         }
+       }
+       count++;
+      }
+     // if no path exists, do not append starting point
+     // append otherwise
+     if(!path.equals("no path"))
+       path = start+path;
+     printShortestTimes(times);
+
 
     // Extra Credit: Code below to print the suggested play path i.e. "2, 4, 3, 5"
+    System.out.println("Suggested play path : ");
+    System.out.println(path);
+    printShortestTimes(times);
   }
 
   /**
